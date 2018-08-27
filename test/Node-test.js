a3e25fff6ee9ba4cf2d8e65deea69739043d4b90
Cole Hart <cole@colehart.com>
2018-08-27 15:07:54 -0600

    Add tests for single and multiple word retrieval

--- test/Node-test.js
+++ test/Node-test.js
@@ -20,11 +20,11 @@ describe('Node', () => {
     assert.equal(node.data, 'sammich');
   });
 
-  // it('should have access to full English alphabet', () => {
-  //   assert.deepEqual(node.letters, ['a', 'b', 'c',
-  //     'd', 'e', 'f', 'g', 'h', 'i',
-  //     'j', 'k', 'l', 'm', 'n', 'o',
-  //     'p', 'q', 'r', 's', 't', 'u',
-  //     'v', 'w', 'x', 'y', 'z'])
-  // });
+  it('should have access to full English alphabet', () => {
+    assert.deepEqual(node.letters, ['a', 'b', 'c',
+      'd', 'e', 'f', 'g', 'h', 'i',
+      'j', 'k', 'l', 'm', 'n', 'o',
+      'p', 'q', 'r', 's', 't', 'u',
+      'v', 'w', 'x', 'y', 'z'])
+  });
 });
--- test/Trie-test.js
+++ test/Trie-test.js
@@ -33,13 +33,47 @@ describe('Trie', () => {
   it('should add new node when taking in a new word', () => {
     trie.insert('hello');
 
-    assert.deepEqual(trie.root, { data: 'hello', next: null });
+    assert.deepEqual(trie.root, { data: 'hello',
+      next: null,
+      letters: ['a', 'b', 'c',
+      'd', 'e', 'f', 'g', 'h', 'i',
+      'j', 'k', 'l', 'm', 'n', 'o',
+      'p', 'q', 'r', 's', 't', 'u',
+      'v', 'w', 'x', 'y', 'z']
+    });
   });
 
   it('should add multiple new nodes', () => {
     trie.insert('hello');
     trie.insert('world');
 
-    assert.deepEqual(trie.root.next, { data: 'world', next: null });
-  })
+    assert.deepEqual(trie.root.next, { data: 'world',
+      next: null,
+      letters: ['a', 'b', 'c',
+      'd', 'e', 'f', 'g', 'h', 'i',
+      'j', 'k', 'l', 'm', 'n', 'o',
+      'p', 'q', 'r', 's', 't', 'u',
+      'v', 'w', 'x', 'y', 'z']
+    });
+  });
+
+  it.skip('should suggest a word based on a prefix', () => {
+    trie.insert('hello');
+    trie.insert('world');
+
+    result1 = trie.suggest('he');
+    result2 = trie.suggest('w');
+
+    assert.deepEqual(result1, ['hello']);
+    assert.deepEqual(result2, ['world']);
+  });
+
+  it.skip('should suggest all matching world based on a prefix', () => {
+    trie.insert('hello');
+    trie.insert('hellen');
+
+    result = trie.suggest('he');
+
+    assert.deepEqual(result, ['hello', 'hellen'])
+  });
 });
