44bc0ea5a393bd081a76234ad996ab5edf5d8b08
Cole Hart <cole@colehart.com>
2018-08-27 13:33:46 -0600

    Add node properties and tests

--- test/Node-test.js
+++ test/Node-test.js
@@ -5,10 +5,26 @@ describe('Node', () => {
   let node;
 
   beforeEach(() => {
-    node = new Node();
+    node = new Node('sammich');
   });
 
   it('should be a class', () => {
     assert.instanceOf(node, Node);
-  })
-})
+  });
+
+  it('should default to a next of null', () => {
+    assert.isNull(node.next);
+  });
+
+  it('should receive data', () => {
+    assert.equal(node.data, 'sammich');
+  });
+
+  it('should have access to full English alphabet', () => {
+    assert.deepEqual(node.letters, ['a', 'b', 'c',
+      'd', 'e', 'f', 'g', 'h', 'i',
+      'j', 'k', 'l', 'm', 'n', 'o',
+      'p', 'q', 'r', 's', 't', 'u',
+      'v', 'w', 'x', 'y', 'z'])
+  });
+});
